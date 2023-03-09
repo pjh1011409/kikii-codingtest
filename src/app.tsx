f77ad5563115197d7d520382f528c7b108abcc6e
ParkJeongHo <pjh31208@naver.com>
2023-03-09 18:31:56 +0900

    Feat(ux): Setup spinner ui with suspense fallback

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,13 @@
 import React, { Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from '../src/layout/layout';
+import { LoadSpinner } from './components/LoadUI/LoadSpinner';
 
 const LoginPage = React.lazy(() => import('./pages/loginPage'));
 const ManagementPage = React.lazy(() => import('./pages/managementPage'));
 
 const App = () => (
-  <Suspense fallback={''}>
+  <Suspense fallback={<LoadSpinner />}>
     <Routes>
       <Route path="/" element={<Navigate replace to="/loginPage" />} />
       <Route path="/loginPage" element={<LoginPage />} />
