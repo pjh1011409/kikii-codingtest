0d94cee3aa740d32347c74cba632bd6a1fc40cad
ParkJeongHo <pjh31208@naver.com>
2023-03-09 16:29:42 +0900

    Feat(root): Create root file

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import App from './app';
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store/store';
+import { ThemeProvider } from '@emotion/react';
+import theme from './styles/theme';
+
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
