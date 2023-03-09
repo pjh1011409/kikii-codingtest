2f2b611cbedf1720b10c7d2f80bfd9c055aef590
ParkJeongHo <pjh31208@naver.com>
2023-03-09 17:20:29 +0900

    Feat: Create root components for navBar's components

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+import DropdownBar from './DropdownBar';
+import NormalNavBar from './NormalNavBar';
 
-const NavBar = () => {
-  return <div>NavBar</div>;
+const Navbar = () => {
+  const [isOpened, setIsOpened] = useState(false);
+
+  return (
+    <NavbarContainer>
+      <NormalNavBar isOpened={isOpened} setIsOpened={setIsOpened} />
+      <DropdownBar isOpened={isOpened} />
+    </NavbarContainer>
+  );
 };
 
-export default NavBar;
+export default Navbar;
+
+const NavbarContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: rgba(234, 236, 251);
+  color: rgba(1, 0, 0);
+  height: 45px;
+  position: relative;
+  z-index: 999;
+`;
