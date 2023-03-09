f03c05b2a03cb5c4ad72b31799774cbecf46a23e
ParkJeongHo <pjh31208@naver.com>
2023-03-09 18:33:12 +0900

    Feat(ux): Toast ui global settings

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store/store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
@@ -12,6 +14,7 @@ root.render(
     <BrowserRouter>
       <Provider store={store}>
         <App />
+        <ToastContainer />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
